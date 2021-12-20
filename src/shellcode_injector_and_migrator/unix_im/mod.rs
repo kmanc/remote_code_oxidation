d576d972e42f2469add0b043df3405a8580276d6
Kevin <kmancxc@gmail.com>
2021-12-20 04:50:49 -0800

    placeholder

--- src/shellcode_injector_and_migrator/unix_im/mod.rs
+++ src/shellcode_injector_and_migrator/unix_im/mod.rs
@@ -1,3 +1,6 @@
-pub fn inject_and_migrate(shellcode: &str) {
+use std::str;
+
+pub fn inject_and_migrate(shellcode: &[u8]) {
+    let shellcode = str::from_utf8(shellcode).unwrap();
     println!("{}", shellcode);
 }
