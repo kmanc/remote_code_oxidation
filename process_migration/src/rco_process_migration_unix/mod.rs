fe5e23c32587591330f225f2f6080558d9f0979d
Kevin <kmancxc@gmail.com>
2022-01-01 12:24:23 -0800

    WIP

--- process_migration/src/rco_process_migration_unix/mod.rs
+++ process_migration/src/rco_process_migration_unix/mod.rs
@@ -1,6 +1,30 @@
-use std::str;
+extern crate libc;
+#[cfg(target_os="linux")]
+use libc::process_vm_readv;
+use std::process::{Command};
+
+const MAX_PID: i32 = 32768;
 
 pub fn inject_and_migrate(shellcode: &[u8]) {
-    let shellcode = str::from_utf8(shellcode).unwrap();
-    println!("{}", shellcode);
+    println!("{}", "this is a thing");
+    let mut pid = 2;
+    // End up changing this to less than MAX_PID
+    while pid < 2000 {
+        let kill = Command::new("kill")
+            .arg("-s")
+            .arg("0")
+            .arg(pid.to_string())
+            .output()
+            .unwrap();
+        match kill.stderr.len() {
+            0 => {
+                // look for ptrace_seize or (preferably!) process_vm_readv and break if found
+                //println!("candidate: {}", &pid);
+                pid += 1
+            },
+            _ => pid += 1
+        }
+    }
+    // Try http://phrack.org/issues/59/12.html but with https://man7.org/linux/man-pages/man2/process_vm_readv.2.html
+    //let test = unsafe { libc::process_vm_readv() };
 }
