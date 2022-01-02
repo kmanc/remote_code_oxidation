6f2bb54439e736e46fe6285e895f4278203e5941
koins <koins@duck.com>
2022-01-01 19:29:06 -0800

    really not sure how to run this thing

--- process_migration/src/rco_process_migration_linux/mod.rs
+++ process_migration/src/rco_process_migration_linux/mod.rs
@@ -1,5 +1,5 @@
 extern crate libc;
-use libc::{process_vm_readv, process_vm_writev};
+use libc::{iovec, process_vm_readv, process_vm_writev};
 use std::process::{Command};
 
 const MAX_PID: i32 = 32768;
@@ -7,8 +7,7 @@ const MAX_PID: i32 = 32768;
 pub fn inject_and_migrate(shellcode: &[u8]) {
     println!("Printing a {}", "thing");
     let mut pid = 2;
-    // End up changing this to less than MAX_PID
-    while pid < 2000 {
+    while pid < MAX_PID {
         let kill = Command::new("kill")
             .arg("-s")
             .arg("0")
@@ -19,11 +18,22 @@ pub fn inject_and_migrate(shellcode: &[u8]) {
             0 => {
                 // look for ptrace_seize or (preferably!) process_vm_readv and break if found
                 //println!("candidate: {}", &pid);
-                pid += 1
+                let buff = vec![0u8, 8];
+                let buffb = vec![0u8, 8];
+                let local_io = unsafe { iovec{
+                    iov_base: std::mem::transmute(&buff),
+                    iov_len: buff.len()}
+                };
+                let remote_io = unsafe { iovec{
+                    iov_base: std::mem::transmute(&buffb),
+                    iov_len: buffb.len()}
+                };
+                let a = unsafe { process_vm_readv(pid, &local_io, local_io.iov_len as u64, &remote_io, remote_io.iov_len as u64, 0) };
+                println!("Pid: {} Readv:{}", pid, a);
+                pid += 1;
             },
             _ => pid += 1
         }
     }
     // Try http://phrack.org/issues/59/12.html but with https://man7.org/linux/man-pages/man2/process_vm_readv.2.html
-    //let test = unsafe { libc::process_vm_readv() };
 }
