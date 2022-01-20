acad1aff8dda020e97f86eefb63271eb89093a01
koins <koins@duck.com>
2022-01-19 23:17:24 -0800

    needed 64 bit too

--- rco_utils/src/lib.rs
+++ rco_utils/src/lib.rs
@@ -13,3 +13,25 @@ pub fn array_to_u32_lit_end(array: &[u8; 4]) -> u32 {
     (array[2] as u32) << 16 |
     (array[3] as u32) << 24
 }
+
+pub fn array_to_u64_big_end(array: &[u8; 8]) -> u64 {
+    (array[0] as u64) << 56 |
+    (array[1] as u64) << 48 |
+    (array[2] as u64) << 40 |
+    (array[3] as u64) << 32 |
+    (array[4] as u64) << 24 |
+    (array[5] as u64) << 16 |
+    (array[6] as u64) << 8  |
+    (array[7] as u64)
+}
+
+pub fn array_to_u64_lit_end(array: &[u8; 8]) -> u64 {
+    (array[0] as u64)       |
+    (array[1] as u64) <<  8 |
+    (array[2] as u64) << 16 |
+    (array[3] as u64) << 24 |
+    (array[4] as u64) << 32 |
+    (array[5] as u64) << 40 |
+    (array[6] as u64) << 48 |
+    (array[7] as u64) << 56
+}
