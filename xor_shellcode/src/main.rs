fcceadddd7a1374a544bfe924320b434cf868cf3
Kevin <kmancxc@gmail.com>
2022-01-30 09:41:59 -0800

    better validation of input data

--- xor_shellcode/src/main.rs
+++ xor_shellcode/src/main.rs
@@ -2,12 +2,11 @@ fn main() {
     let win_shellcode = rco_config::WINDOWS_SHELLCODE;
     let lin_shellcode = rco_config::LINUX_SHELLCODE;
     let key = rco_config::XOR_KEY;
-    if key.len() > win_shellcode.len() || key.len() > lin_shellcode.len() {
-        panic!("Error: XOR_KEY length is larger than WINDOWS_SHELLCODE, LINUX_SHELLCODE, or both");
-    }
 
-    let win_key: &[u8] = &rco_utils::equalize_slice_len(key, win_shellcode)[..];
-    let win_output = rco_utils::xor_u8_slices(win_key, win_shellcode);
+    let equalilzed = rco_utils::equalize_slice_len(key, win_shellcode);
+    let win_key: &[u8] = &equalilzed.0[..];
+    let win_shellcode: &[u8] = &equalilzed.1[..];
+    let win_output = rco_utils::xor_u8_slices(win_key, win_shellcode).unwrap();
     let mut win_print = "".to_owned();
     for byte in win_output.iter() {
         let formatted = format!("{byte:#04x}, ");
@@ -19,8 +18,10 @@ fn main() {
 
     println!();
 
-    let lin_key: &[u8] = &rco_utils::equalize_slice_len(key, lin_shellcode)[..];
-    let lin_output = rco_utils::xor_u8_slices(lin_key, lin_shellcode);
+    let equalilzed = rco_utils::equalize_slice_len(key, lin_shellcode);
+    let lin_key: &[u8] = &equalilzed.0[..];
+    let lin_shellcode: &[u8] = &equalilzed.1[..];
+    let lin_output = rco_utils::xor_u8_slices(lin_key, lin_shellcode).unwrap();
     let mut lin_print = "".to_owned();
     for byte in lin_output.iter() {
         let formatted = format!("{byte:#04x}, ");
