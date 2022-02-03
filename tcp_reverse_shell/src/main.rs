fee122aa39057d255641a578dfb5bc9402199d1e
koins <koins@duck.com>
2022-02-03 14:46:45 -0800

    better code comments

--- tcp_reverse_shell/src/main.rs
+++ tcp_reverse_shell/src/main.rs
@@ -1,9 +1,10 @@
-// Load the appropriate operating system's implementation
+// Load Linux implementation if the target OS is Linux
 #[cfg(target_os = "linux")]
 mod rco_reverse_shell_linux;
 #[cfg(target_os = "linux")]
 use rco_reverse_shell_linux::shell;
 
+// Load Windows implementation if the target OS is Windows
 #[cfg(windows)]
 mod rco_reverse_shell_windows;
 #[cfg(windows)]
