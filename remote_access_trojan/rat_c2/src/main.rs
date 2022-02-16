9f2750dee8fe8951947cc5975695c4216d1e2d42
koins <koins@duck.com>
2022-02-15 22:42:54 -0800

    just getting protobufs up and running ish

--- remote_access_trojan/rat_c2/src/main.rs
+++ remote_access_trojan/rat_c2/src/main.rs
@@ -1,3 +1,16 @@
+use prost::Message;
+
+pub mod rat {
+    include!(concat!(env!("OUT_DIR"), "/rat.items.rs"));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut test = rat::Test::default();
+    test.words = "word string".to_string();
+    test.thoughts = "thought string".to_string();
+    let mut buf = Vec::new();
+    buf.reserve(test.encoded_len());
+    test.encode(&mut buf).unwrap();
+    println!("{test:?}");
+    println!("{buf:?}");
 }
